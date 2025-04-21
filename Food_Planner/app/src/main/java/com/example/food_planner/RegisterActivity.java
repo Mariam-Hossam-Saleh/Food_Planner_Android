b34cdd23f1634b76837c90e63fe292954bf6c313
Mariam <mariam.hossam.keshk@gmail.com>
2025-04-21 03:08:14 +0200

    Feature: creating Register Screen and Activity

--- Food_Planner/app/src/main/java/com/example/food_planner/RegisterActivity.java
+++ Food_Planner/app/src/main/java/com/example/food_planner/RegisterActivity.java
@@ -1,6 +1,13 @@
 package com.example.food_planner;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +17,47 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText edtPassword;
+    Button btnSkip;
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtPassword = findViewById(R.id.edtPassword);
+        btnSkip = findViewById(R.id.btnSkip);
+        edtPassword.setOnTouchListener((v, event) -> {
+            final int DRAWABLE_RIGHT = 2;
+
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= (edtPassword.getRight()
+                        - edtPassword.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+
+                    int selection = edtPassword.getSelectionEnd();
+
+                    if ((edtPassword.getInputType() & InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD)
+                            == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                        edtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        edtPassword.setCompoundDrawablesWithIntrinsicBounds(R.drawable.key, 0, R.drawable.invisibile, 0);
+                    } else {
+                        edtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                        edtPassword.setCompoundDrawablesWithIntrinsicBounds(R.drawable.key, 0, R.drawable.visibile, 0);
+                    }
+
+                    edtPassword.setSelection(selection);
+                    return true;
+                }
+            }
+            return false;
         });
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
